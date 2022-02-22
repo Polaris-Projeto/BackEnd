a1490b974fb20549539fc5a1e013ac8988d59068
Leonardo E S Rodrigues <leonardo.elias0908@gmail.com>
2022-02-22 12:59:38 -0300

    Adicionando get set

--- src/main/java/com/generation/projetopolaris/model/Tema.java
+++ src/main/java/com/generation/projetopolaris/model/Tema.java
@@ -17,6 +17,22 @@ public class Tema {
 	@NotBlank(message="O atributo é obrigatorio")
 	private String descricao;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
 
 	
 	
