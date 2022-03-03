9423e70d5a57e8496155a96ac6e6dd55514ddfa7
Leonardo E S Rodrigues <leonardo.elias0908@gmail.com>
2022-03-03 12:51:53 -0300

    Criando Controller  Usuario e Postagem

---
+++ src/main/java/com/generation/projetopolaris/controller/UsuarioController.java
@@ -0,0 +1,70 @@
+package com.generation.projetopolaris.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.projetopolaris.model.Usuario;
+import com.generation.projetopolaris.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+		return usuarioRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Usuario>> getByNome(@PathVariable String nome) {
+		return ResponseEntity.ok(usuarioRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+
+	@PostMapping
+	public ResponseEntity<Usuario> postUsuario(@Valid @RequestBody Usuario usuario) {
+		if (usuarioRepository.existsById(usuario.getId())) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+		}
+		return ResponseEntity.badRequest().build();
+	}
+
+	@PutMapping
+	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
+		if (usuarioRepository.existsById(usuario.getId())) {
+			return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario));
+		}
+		return ResponseEntity.badRequest().build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deletePostagem(@PathVariable Long id) {
+		return usuarioRepository.findById(id).map(resposta -> {
+			usuarioRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+}
